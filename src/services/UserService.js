5888797666d3ecafa50ab4a36971183416cf4fab
Pavlo Klymchuk <pavlo95klymchuk@gmail.com>
2021-11-30 00:22:48 +0200

    fixed concolelog

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,7 +3,6 @@ import { $host } from '../api';
 export default class UserService {
     static signin = async (userData) => {
         const { data } = await $host.post('/api/v1/security/login', userData);
-        console.log(data)
         localStorage.setItem('token', data.token);
         return data;
     };
